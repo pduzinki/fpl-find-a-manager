6f12b66c15b0db1645788d1c80feb44273529e8f
pawel duzinkiewicz <pawel.duzinkiewicz@live.com>
2021-06-13 09:19:04 +0200

    add user input loop

--- cmd/cli/main.go
+++ cmd/cli/main.go
@@ -3,5 +3,18 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("hello from fpl-find-a-manager cli")
+	fmt.Println("Welcome to 'Find a manager' fpl app!")
+
+	for {
+		fmt.Println("Please type the name of the manager you're looking for, or [ctrl+c] to exit:")
+
+		var nameInput string
+		fmt.Scanln(&nameInput)
+
+		// TODO remove later
+		fmt.Println("hello " + nameInput)
+		fmt.Println("No managers found!")
+
+		fmt.Println("--------------------------------")
+	}
 }
