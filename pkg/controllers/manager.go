6ba6ce11f19004731d2ee70d0a80ea72f34d09e6
pawel duzinkiewicz <pawel.duzinkiewicz@live.com>
2021-06-30 20:27:52 +0200

    add validator and gorm structs for manager model

--- pkg/controllers/manager.go
+++ pkg/controllers/manager.go
@@ -1,23 +1,49 @@
 package controllers
 
-import "fpl-find-a-manager/pkg/models"
+import (
+	"fmt"
+	"fpl-find-a-manager/pkg/models"
+	"fpl-find-a-manager/pkg/wrapper"
+)
 
 //
 type ManagerController struct {
+	ms models.ManagerService
+	w  wrapper.Wrapper
 }
 
 //
 func NewManagerController(ms models.ManagerService) *ManagerController {
-	return &ManagerController{}
+	return &ManagerController{
+		ms: ms,
+		w:  wrapper.NewWrapper(),
+	}
 }
 
 // MatchManagersByName returns managers that name contains given string.
 func (mc *ManagerController) MatchManagersByName(name string) ([]models.Manager, error) {
-	return nil, nil
+	managers, err := mc.ms.MatchManagersByName(name)
+	if err != nil {
+		return nil, err
+	}
+
+	return managers, nil
 }
 
 // AddManagers constantly queries FPL API wrapper and keeps managers db up-to-date.
 // Should be run in goroutine.
 func (mc *ManagerController) AddManagers() {
+	wm, err := mc.w.GetManager(1239)
+	if err != nil {
+		fmt.Println("failed to get manager via fpl api")
+	}
 
+	am := models.Manager{
+		FplID:    wm.ID,
+		FullName: fmt.Sprintf("%s %s", wm.FirstName, wm.LastName),
+	}
+
+	mc.ms.AddManager(&am)
+
+	fmt.Println(wm)
 }
--- pkg/models/service.go
+++ pkg/models/service.go
@@ -8,15 +8,22 @@ import (
 	"gorm.io/gorm/logger"
 )
 
-type ManagerService interface {
+//
+type ManagerDB interface {
 	AddManager(manager *Manager) error
 	MatchManagersByName(name string) ([]Manager, error)
 }
 
-type managerService struct {
-	db *gorm.DB
+//
+type ManagerService interface {
+	ManagerDB
 }
 
+type managerService struct {
+	ManagerDB
+}
+
+//
 func NewManagerService() (ManagerService, error) {
 	db, err := gorm.Open(sqlite.Open("test.db"), &gorm.Config{
 		Logger: logger.Default.LogMode(logger.Silent),
@@ -27,19 +34,64 @@ func NewManagerService() (ManagerService, error) {
 
 	db.AutoMigrate(&Manager{})
 
+	mg := newManagerGorm(db)
+	mv := newManagerValidator(mg)
+
 	return &managerService{
-		db: db,
+		ManagerDB: mv,
 	}, nil
 }
 
+//
 func (ms *managerService) AddManager(manager *Manager) error {
-	return ms.db.Create(&manager).Error
+	return ms.ManagerDB.AddManager(manager)
 }
 
+//
 func (ms *managerService) MatchManagersByName(name string) ([]Manager, error) {
+	return ms.ManagerDB.MatchManagersByName(name)
+}
+
+// ----------------------------------------------------------------
+type managerValidator struct {
+	ManagerDB
+}
+
+func newManagerValidator(m ManagerDB) *managerValidator {
+	return &managerValidator{
+		ManagerDB: m,
+	}
+}
+
+func (mv *managerValidator) AddManager(manager *Manager) error {
+	// TODO add validations
+	return mv.ManagerDB.AddManager(manager)
+}
+
+func (mv *managerValidator) MatchManagersByName(name string) ([]Manager, error) {
+	// TODO add validations
+	return mv.ManagerDB.MatchManagersByName(name)
+}
+
+// ----------------------------------------------------------------
+type managerGorm struct {
+	db *gorm.DB
+}
+
+func newManagerGorm(db *gorm.DB) *managerGorm {
+	return &managerGorm{
+		db: db,
+	}
+}
+
+func (mg *managerGorm) AddManager(manager *Manager) error {
+	return mg.db.Create(&manager).Error
+}
+
+func (mg *managerGorm) MatchManagersByName(name string) ([]Manager, error) {
 	managers := make([]Manager, 0)
 
-	err := ms.db.Where("full_name LIKE ?", fmt.Sprintf("%%%s%%", name)).
+	err := mg.db.Where("full_name LIKE ?", fmt.Sprintf("%%%s%%", name)).
 		Find(&managers).Error
 
 	return managers, err
